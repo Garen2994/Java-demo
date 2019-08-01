e37eb880f186b191fb13b667710a4687a7727255
Garen <garen2994@hotmail.com>
2019-08-01 16:07:21 +0800

    day01 update

---
+++ src/garen/java/demo/day01.java
@@ -0,0 +1,30 @@
+package garen.java.demo;
+
+import java.util.Arrays;
+
+public class day01 {
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        for (int i = 0; i < a.length; i++)
+            a[i] = i;
+//      for (int i = 0; i < a.length; i++)  //general output
+//          System.out.println(a[i]);
+        for (int element : a)             //for each loop output
+            System.out.println(element);
+        System.out.println(Arrays.toString(a)); //class Arrays toString() output
+        int[] b = new int[]{2, 5, 3, 9, 7, 17, 13, 11, 19};
+
+        int[] copied_b = Arrays.copyOf(b, 2 * b.length); //copy array
+        System.out.println(Arrays.toString(copied_b));
+
+        int[][] aa = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        for (int[] row:aa)    //output 2-D array
+            for (int value:row)
+                System.out.println(value);
+        System.out.println(Arrays.deepToString(aa)); //quick output
+    }
+}
