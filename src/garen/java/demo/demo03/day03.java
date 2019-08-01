79c6018ee76e6889ff94ee1d3206cfdfaac11ddd
Garen <garen2994@hotmail.com>
2019-08-01 23:08:07 +0800

    demo03 update

---
+++ src/garen/java/demo/demo03/day03.java
@@ -0,0 +1,15 @@
+package garen.java.demo.demo03;
+
+public class day03 {
+    public static void main(String[] args) {
+        int area = getArea(5,6);
+        System.out.println("面积是："+area);
+    }
+
+    public static int getArea(int w , int h) {
+
+        //int area = w * h;
+        return  w * h;
+
+    }
+}
