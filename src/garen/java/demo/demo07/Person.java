afb71a59a127fc91a96f34fb60d9e13308600ffd
Garen <garen2994@hotmail.com>
2019-08-15 21:23:01 +0800

    day12 notebook update

--- src/garen/java/demo/demo07/Person.java
+++ src/garen/java/demo/demo07/Person.java
@@ -1,7 +1,16 @@
 package garen.java.demo.demo07;
 
-class Person {
+public class Person {
 
+    private int age;
+    private String name;
 
+    Person(int a, String nm) {
+        age = a;
+        name = nm;
+    }
 
+    public void speak() {
+        System.out.println("name = " + name + ",age" + age);
+    }
 }
---
+++ src/garen/java/demo/demo07/PersonDemo.java
@@ -0,0 +1,11 @@
+package garen.java.demo.demo07;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+
+        Person p1 = new Person(23, "Garen");
+        p1.speak();
+
+    }
+
+}
