cc295a94b223253b73794b529036c5a15ed9d6e6
Garen <garen2994@hotmail.com>
2019-08-26 15:10:05 +0800

    :star2: day20 play cards practice

--- src/garen/java/demo/demo11/DDZPractice/DDZPractice.java
+++ src/garen/java/demo/demo11/DDZPractice/DDZPractice.java
@@ -90,7 +90,7 @@ public class DDZPractice {
 
         for (Integer key : array) {
             String value = map.get(key);
-            System.out.println(key + " " + value);
+            System.out.println(value);
         }
 
 
