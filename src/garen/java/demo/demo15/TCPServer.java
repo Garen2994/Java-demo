b34f37d9ecc7edaeb84d052f80d016f73230f450
Garen <garen2994@hotmail.com>
2019-08-31 20:03:11 +0800

    :sparkles: day-Add Web update

---
+++ src/garen/java/demo/demo15/TCPServer.java
@@ -0,0 +1,22 @@
+package garen.java.demo.demo15;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8888);
+        Socket socket = server.accept();
+        InputStream in = socket.getInputStream();
+        byte[] b = new byte[1024];
+        int len = in.read(b);
+        System.out.println(new String(b, 0, len));
+        OutputStream out = socket.getOutputStream();
+        out.write("Hi!Thanks".getBytes());
+        socket.close();
+        server.close();
+    }
+}
